9f2e2019692bdbc43d041b7cc6197d5a4b6ef76a
Osekobo <collinsboseko2005@gmail>
2025-06-09 17:38:02 +0300

    09/06/25

---
+++ database.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+conn = psycopg2.connect(user="postgres", password="12039",
+                        host="localhost", port="5432", database="lelo")
+
+cur = conn.cursor()
+
+cur.execute("select * from boy")
+data = cur.fetchall()
+print(data)
