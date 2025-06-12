89b391152a4a3a4651b09bf29bca1006312b3914
Osekobo <collinsboseko2005@gmail>
2025-06-12 12:38:18 +0300

    12/06/25

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 import psycopg2
+from datetime import datetime
 
 conn = psycopg2.connect(user="postgres", password="12039",
-                        host="localhost", port="5432", database="postgres")
+                        host="localhost", port="5432", database="my_duka")
 
 cur = conn.cursor()
 
-cur.execute("select * from products")
-data = cur.fetchall()
-print(data)
+# products
 
 
 def get_products():
@@ -15,27 +14,48 @@ def get_products():
     products = cur.fetchall()
     return products
 
+# sales
+
 
 def get_sales():
-    query = "select * from sales"
-    cur.execute(query)
+    cur.execute("select * from sales")
     sales = cur.fetchall()
     return sales
 
 
+product_data = get_products()
+sales_data = get_sales()
+
+# print("Product data", product_data)
+# print("Sales data", sales_data)
+
+# method 1 inserting products
 def insert_products(values):
     insert_query = "insert into products(name,buying_price,selling_price)values(%s,%s,%s)"
     cur.execute(insert_query, values)
     conn.commit()
 
-
+# method 2 inserting products
 def insert_products(values):
-    cur.execute(
-        f"insert into products(name,buying_price,selling_price)values{values}")
+    cur.execute(f"insert into products(name,buying_price,selling_price)values{values}")
     conn.commit()
 
+product_value1 = ('cup',40,80)
+product_value2 = ('spoon',50,100)
 
+insert_products(product_value1)
+insert_products(product_value2)
+products = get_products()
+print(products)
+
+# inserting sales
+current_datetime = str(datetime.now())
+print(current_datetime)
 def insert_sales(values):
-    insert_sales = "insert into sales(pid,quantity,created_at)values(%s,%s,%s)"
-    cur.execute(insert_sales, values)
+    cur.execute(f"insert into sales(pid,quantity,created_at)values{values}")
     conn.commit()
+
+sale_values = (2,8,current_datetime)    
+insert_sales(sale_values)
+sales = get_sales()
+print(sales)
